3a6c19a471b23144f301c1057bd70d59652b938c
L6Echo <haraldhomolka@Alfred.local>
2015-11-22 15:07:04 +0100

    api to add elements

--- androidApp/app/src/main/java/com/example/raumdapp/BleScannerService.java
+++ androidApp/app/src/main/java/com/example/raumdapp/BleScannerService.java
@@ -10,6 +10,15 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
 
 public class BleScannerService extends IntentService {
 
@@ -67,5 +76,95 @@ public class BleScannerService extends IntentService {
         EstimoteManager.stop();
         super.onDestroy();
         Log.d(LOG_TAG,"destroyed");
+    }
+
+
+
+    public void addRoom(Room room){
+
+        if(room == null) throw new IllegalStateException();
+
+        EstimoteManager.getRoomMap().put(room.getBeaconId(), room);
+
+        Log.d(LOG_TAG,"Added room "+room.getName());
+    }
+
+    public void removeRoom(Room room){
+
+        if(room == null) throw new IllegalStateException();
+
+        EstimoteManager.getRoomMap().remove(room.getBeaconId());
+
+
+        Log.d(LOG_TAG, "Removed room " + room.getName());
+
+    }
+
+    public void updateRoom(Room room){
+
+        if(room == null) throw new IllegalStateException();
+
+        Room room1 = EstimoteManager.getRoomMap().get(room.getBeaconId());
+
+        if(room1 == null) throw new IllegalStateException();
+
+        room1.setDesiredTemperature(room.getDesiredTemperature());
+
+        Log.d(LOG_TAG,"Updated room "+room.getName());
+    }
+
+    public Collection<Room> getAllRooms(){
+        return EstimoteManager.getRoomMap().values();
+    }
+
+
+    private void persist(){
+
+        try {
+            InternalStorage.writeObject(this,"data", EstimoteManager.getRoomMap());
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "persisting failed",e);
+        }
+
+    }
+
+    private void load(){
+
+        try {
+
+            HashMap<String, Room> map = (HashMap<String, Room>) InternalStorage.readObject(this,"data");
+
+        } catch (IOException e) {
+
+            Log.e(LOG_TAG, "read data failed",e);
+
+        } catch (ClassNotFoundException e) {
+
+            Log.e(LOG_TAG, "read data failed", e);
+
+        }
+
+    }
+
+
+    public static final class InternalStorage{
+
+        private InternalStorage() {}
+
+        public static void writeObject(Context context, String key, Object object) throws IOException {
+            FileOutputStream fos = context.openFileOutput(key, Context.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(object);
+            oos.close();
+            fos.close();
+        }
+
+        public static Object readObject(Context context, String key) throws IOException,
+                ClassNotFoundException {
+            FileInputStream fis = context.openFileInput(key);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Object object = ois.readObject();
+            return object;
+        }
     }
 }
--- androidApp/app/src/main/java/com/example/raumdapp/MainActivity.java
+++ androidApp/app/src/main/java/com/example/raumdapp/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.raumdapp;
 
-import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
-import com.estimote.sdk.EstimoteSDK;
-
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
+
+    private BleScannerService bleScannerService;
+    public boolean mBound;
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Intent intent = new Intent(this, BleScannerService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +36,37 @@ public class MainActivity extends AppCompatActivity {
         adapter.add(new Room("Kitchen", "kitchen", "123", 24));
 
         v.setAdapter(adapter);
+
+
     }
+
+    private void onServiceConnected(){
+
+        // After this callback service is available
+
+        bleScannerService.addRoom(new Room("Kitchen","kitchen","123",24));
+
+    }
+
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            BleScannerService.LocalBinder binder = (BleScannerService.LocalBinder) service;
+            bleScannerService = binder.getService();
+            mBound = true;
+            MainActivity.this.onServiceConnected();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+
 }
